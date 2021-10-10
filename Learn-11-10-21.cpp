647039a7827fb8f334395e153bd988e199e754c3
thanet_y@yahoo.com <thanet_y@yahoo.com>
2021-10-10 13:44:01 +0700

    more code

--- Learn-11-10-21.cpp
+++ Learn-11-10-21.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
+using namespace std;
 int main()
 {
-int a = 13  ;
-int b = 7   ;
-int max ;
+    int a = 13  ;
+    int b = 7   ;
+    int max ;
+    if (a > max)
+    {
+        max = a     ;
+    }
+
+    cout << " max value is " << max << endl ;
 
 return 0    ;
 }
